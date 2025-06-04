d8adbde5849c6f7b50141d38cde069ec4c2ca641
Dhuruv Kumar <dhruvdk02@gmail.com>
2025-06-04 18:56:23 +0530

    Added questions 7,8,9

---
+++ Assignment-3/question-7.js
@@ -0,0 +1,8 @@
+function doubleTheNumber(){
+    let i=1;
+    while(i<4096){
+        i*=2;
+        console.log(i); 
+    }
+}
+doubleTheNumber()
---
+++ Assignment-3/question-8.js
@@ -0,0 +1,22 @@
+// Using for loop
+
+for(let i=1;i<=20;i++){
+    console.log(i);
+}
+
+// using do while loop
+
+let i=1;
+do{
+    console.log(i);
+    i++;    
+}while(i<=20);
+
+// while loop
+
+let j=1;
+while(j<=20)
+{
+    console.log(j);
+    j++;    
+}
