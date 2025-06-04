565fcd35aea56d21743ec2f1f9b13e5fcfbfc2f4
Dhuruv Kumar <dhruvdk02@gmail.com>
2025-06-04 19:00:27 +0530

    Updated the code for question-4

--- Assignment-3/question-4.js
+++ Assignment-3/question-4.js
@@ -1,13 +1,13 @@
 // Q4. Write a program to print all even number first and then all odd numbers.
 
-    let [limit]=process.argv.slice(2);
+    let [n]=process.argv.slice(2);
 
-    for(let i=1;i<=limit;i++){
+    for(let i=1;i<=n;i++){
         if(i%2===0){
             console.log(i); 
         }
     }
-    for(let i=1;i<=limit;i++){
+    for(let i=1;i<=n;i++){
         if(i%2!==0){
             console.log(i); 
         }
