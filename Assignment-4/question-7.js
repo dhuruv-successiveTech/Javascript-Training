5c48fec05d30648595a4e55f572dad9d73e2cda6
Dhuruv Kumar <dhruvdk02@gmail.com>
2025-06-06 16:25:35 +0530

    Changed naming convention

--- Assignment-4/question-7.js
+++ Assignment-4/question-7.js
@@ -6,17 +6,17 @@ function indexOfDuplicates(arr) {
   let elements = {}; 
   let duplicates = []; 
   for (let i = 0; i < arr.length; i++) {
-    let value = arr[i];
-    if (elements[value] === undefined) {
-      elements[value] = i; // First time seeing this value
-    } else {
-      if (!duplicates.includes(elements[value])) {
-        duplicates.push(elements[value]); // Add first occurrence index
+    let key = arr[i];
+    if (elements[key] === undefined) {
+      elements[key] = i; // First time seeing this value
+    } 
+    else {
+      if (!duplicates.includes(elements[key])) {
+        duplicates.push(elements[key]); // Add first occurrence index
       }
     }
   }
   console.log(duplicates);
 }
 
-// Example usage
 indexOfDuplicates([1, 1, 1, 74, 2, 3, 4, 9, 9, 6, 6, 6, 100, 100]);
