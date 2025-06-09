8d935ebcbd92d1c429972b0ec0d36d9fc10bbb4c
Dhuruv Kumar <dhruvdk02@gmail.com>
2025-06-10 00:29:23 +0530

    Added question files in Assignment-5

---
+++ Assignment-5/question-4.js
@@ -0,0 +1,36 @@
+const obj = {
+  name: "Raj",
+  age: 22,
+  address: {
+    city: "Noida",
+  },
+  subject:[
+    {sub1:"Physics"},
+    {sub2:"chemistry"}
+  ]
+};
+
+console.log(obj);
+
+const deepCloneOfObjectOrArrays = (inputObj) => {
+    if(typeof(inputObj)!=='object'){
+      return inputObj;
+    }
+    let deepObj;
+    if(Array.isArray(inputObj))
+    {
+      deepObj=[];
+      inputObj.map((item,index)=>deepObj[index]=deepCloneOfObjectOrArrays(item))
+    }
+    else{
+      deepObj={};
+      Object.keys(inputObj).forEach(key=>deepObj[key]=deepCloneOfObjectOrArrays(inputObj[key]))
+    }
+    return deepObj;
+};
+
+const cloned = deepCloneOfObjectOrArrays(obj);
+// cloned.name="Rashi";
+cloned.subject[0].sub1="Rashi";
+console.log(cloned);
+
