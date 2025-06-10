0cd0d7f5aa45e107e113cc979efac02c24765245
Dhuruv Kumar <dhruvdk02@gmail.com>
2025-06-10 13:14:20 +0530

    Added question-1

--- Assignment-6/question-1.js
+++ Assignment-6/question-1.js
@@ -1,14 +1,32 @@
-class BankAccount{
-    BankAccount(){
-        
+class BankAccount {
+  constructor(balance, owner) {
+    this.balance = balance;
+    this.owner = owner;
+  }
+  deposit(depositMoney) {
+    if (depositMoney > 0) {
+      this.balance += depositMoney;
+      console.log(`Your account is credited with amount ${depositMoney}`);
+    } else {
+      console.log(`Please enter an appropriate amount`);
+      return;
     }
-    deposit(){
-
+  }
+  withdraw(withdrawMoney) {
+    if (withdrawMoney > 0 && withdrawMoney<this.balance) {
+      this.balance -= withdrawMoney;
+      console.log(`Your account is debited with amount ${withdrawMoney}`);
+    } else {
+      console.log(`Your balance is low`);
+      return;
     }
-    withdraw(){
-
-    }
-    displayBalance(){
-
-    }
+  }
+  displayBalance() {
+    console.log(`Your balance is ${this.balance}`);
+    
+  }
 }
+
+
+const myAccount = new BankAccount(4000,"Dhuruv")
+myAccount.withdraw(5000)
