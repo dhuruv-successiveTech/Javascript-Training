757fb4a76453670082beb813a4cb78de3e23b9a8
Dhuruv Kumar <dhruvdk02@gmail.com>
2025-06-13 01:15:25 +0530

    Added question 1 to 6 in Assignment 8

---
+++ Assignment-8/question-2.js
@@ -0,0 +1,15 @@
+// Q2. let myObject = { name: 'John', age: 30 };
+// for (property in myObject) {
+//    console.log(property + ': ' + myObject.property);
+// }
+
+// Ans. 
+
+// We should use bracket notation to access the properties of the object. 
+
+// Corrected Code
+
+const myObject = { name: 'John', age: 30 };
+for (const property in myObject) {
+   console.log(property + ': ' + myObject[property]);
+}
