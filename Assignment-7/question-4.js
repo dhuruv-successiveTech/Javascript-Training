e848b35eb84b16ed2b743a4a26ef590728a4c609
Dhuruv Kumar <dhruvdk02@gmail.com>
2025-06-13 12:35:51 +0530

    Made changes in q 4 and 5

--- Assignment-7/question-4.js
+++ Assignment-7/question-4.js
@@ -1,3 +1,5 @@
+// Q4. Write a program to implement a Promise-based rate limiter, that limits the number of concurrent requests to a certain number
+
 class RateLimiter {
   constructor(limit) {
     this.limit = limit; // Maximum number of concurrent tasks
--- Assignment-7/question-5.js
+++ Assignment-7/question-5.js
@@ -1,4 +1,4 @@
-// Write a program to implement a Promise-based task queue, that processes tasks in a specified order, with a specified concurrency limit
+// Q5. Write a program to implement a Promise-based task queue, that processes tasks in a specified order, with a specified concurrency limit
 
 // Ans.
 
